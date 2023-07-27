f56d9c9ef172eb97e18fe25dcb043ab55bc36166
OwlHD <m.a.flores02@gmail.com>
2023-07-27 16:11:58 -0700

    featured pup and styling added

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,37 @@ import {puppyList} from './data.js'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
-  console.log('puppyList: ', puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+  // console.log('puppyList: ', puppyList)
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  // console.log('featured pup: ',featuredPup)
 
   return (
     <>
       <div className='app'>
         {
+          <div className='pupDiv'>
+          {
           puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>
-          })
+            return <p className='pupName' onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+            })
+          }
+          </div>
         }
+        {
+        featPupId && (
+          <div className='pupCard'>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+              <li> <img src={featuredPup.photo} /> </li>
+            </ul>
+          </div>
+        )
+        }
+        
       </div>
     </>
   )
--- src/data.js
+++ src/data.js
@@ -6,6 +6,7 @@ export const puppyList = [
       isCute: true,
       age: 5,
       ownerId: 1,
+      photo: 'https://www.dailydogdiscoveries.com/wp-content/uploads/2016/02/wagging-tail.png',
       tricks: [
         { id: 2, title: "Lay Down" },
         { id: 3, title: "Jump through flaming hoop" },
@@ -18,6 +19,7 @@ export const puppyList = [
       isCute: true,
       age: 6,
       ownerId: 1,
+      photo: 'https://media.istockphoto.com/photos/aggressive-angry-dog-picture-id542218056?k=6&m=542218056&s=612x612&w=0&h=jSawPE-i2QHM-6i1YVCiDfuu4nFSTcAlvuIepnIUFk0=',
       tricks: [],
     },
     {
@@ -27,6 +29,7 @@ export const puppyList = [
       isCute: true,
       age: 6,
       ownerId: 2,
+      photo: 'https://i.huffpost.com/gen/1179372/images/o-DOG-GLASSES-facebook.jpg',
       tricks: [{ id: 1, title: "Sit" }],
     },
     {
@@ -36,6 +39,7 @@ export const puppyList = [
       isCute: true,
       age: 4,
       ownerId: 2,
+      photo: 'https://i.pinimg.com/originals/70/94/0d/70940d16feda853ef2e86f127ff4bbb7.jpg',
       tricks: [{ id: 2, title: "Lay Down" }],
     },
     {
@@ -45,6 +49,7 @@ export const puppyList = [
       isCute: true,
       age: 7,
       ownerId: 3,
+      photo: 'https://i.redd.it/1uw76vge45i41.jpg',
       tricks: [],
     },
     {
@@ -54,6 +59,7 @@ export const puppyList = [
       isCute: true,
       age: 1,
       ownerId: 3,
+      photo: 'https://i.pinimg.com/736x/4f/64/ba/4f64ba1c7f17f1cc5ab38029f224189a.jpg',
       tricks: [],
     },
     {
@@ -63,6 +69,7 @@ export const puppyList = [
       isCute: true,
       age: 2,
       ownerId: 3,
+      photo: 'https://i.imgflip.com/1u0wd1.jpg',
       tricks: [],
     },
   ];
